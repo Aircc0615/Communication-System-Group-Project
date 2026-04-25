e2175bed1cd86d50ac3001ea47341673e60d64dd
thewebheadflamez <thewebheadflamez@gmail.com>
2026-04-24 23:32:57 -0700

    push source file
    finished Message.java

--- src/networking/Message.java
+++ src/networking/Message.java
@@ -12,6 +12,20 @@ public class Message implements Serializable { //serialize takes the object and 
     protected String text;
     protected Date date;
     protected Instant timeStamp;
+    
+    private String username;
+    private String password;
+    private boolean success;
+    private int userId;
+    
+    private String chatList;
+    private String chatUserList;
+    private String chatId;
+    private String selectedUserId;
+    private String messageContent;
+    private String chatType;
+    private String memberList;
+    private String chatLog;
 
     public Message(){
         id = count++;
@@ -21,6 +35,20 @@ public class Message implements Serializable { //serialize takes the object and 
         this.text = "Undefined";
         date = new Date();
         timeStamp = Instant.now();
+        
+        username = "";
+        password = "";
+        success = false;
+        userId = -1;
+        
+        chatList = "";
+        chatUserList = "";
+        chatId = "";
+        selectedUserId = "";
+        messageContent = "";
+        chatType = "";
+        memberList = "";
+        chatLog = "";
     }
 
     public Message(MainType mainType, SubType subType, Status status, String text){
@@ -31,6 +59,20 @@ public class Message implements Serializable { //serialize takes the object and 
         this.text = text;
         date = new Date();
         timeStamp = Instant.now();
+        
+        username = "";
+        password = "";
+        success = false;
+        userId = -1;
+        
+        chatList = "";
+        chatUserList = "";
+        chatId = "";
+        selectedUserId = "";
+        messageContent = "";
+        chatType = "";
+        memberList = "";
+        chatLog = "";
     }
 
     public MainType getType(){
@@ -56,6 +98,33 @@ public class Message implements Serializable { //serialize takes the object and 
     public Date getDate() { return date; }
 
     public Instant getTimeStamp() { return timeStamp; }
+    
+    public int getCount() { return count; }
+    
+    public String getUsername() { return username; }
+
+    public String getPassword() { return password; }
+
+    public boolean getSuccess() { return success; }
+
+    public int getUserId() { return userId; }
+
+    public String getChatList() { return chatList; }
+
+    public String getChatUserList() { return chatUserList; }
+
+    public String getChatId() { return chatId; }
+
+    public String getSelectedUserId() { return selectedUserId; }
+
+    public String getMessageContent() { return messageContent; }
+
+    public String getChatType() { return chatType; }
+
+    public String getMemberList() { return memberList; }
+
+    public String getChatLog() { return chatLog; }
+    
 }
 
 
