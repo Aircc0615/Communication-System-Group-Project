48a24fa1bb00946aa13534691f54a0db1afcf1a4
SheaPettit <spettit2@horizon.csueastbay.edu>
2026-04-27 00:06:14 -0700

    Fixed synchronization in chat list for the (hopefully) last time

--- src/chat/ChatList.java
+++ src/chat/ChatList.java
@@ -1,134 +1,151 @@
 package chat;
+
 import java.time.Instant;
 
 //might need to add a semaphore for addchat/deletechat synchronization
 public class ChatList {
 	private Chat[] chats;
 	private int numChats;
-	
+	private Object writeMutex;
+
 	public ChatList() {
-		//default chat size
+		// default chat size
 		chats = new Chat[8];
 		numChats = 0;
+		writeMutex = new Object();
 	}
-	
 
-	//inserts a chat in the array based on the order of the timestamp
-	public synchronized void addChat(Chat chat) {
-		//makes more space if needed (2x)
-		if(numChats >= chats.length) {
-			Chat[] newChats = new Chat[chats.length * 2];
-			for(int i = 0; i <  chats.length; i++) {
-				newChats[i] = chats[i];
+	// inserts a chat in the array based on the order of the timestamp
+	public void addChat(Chat chat) {
+		synchronized (writeMutex) {
+			Chat[] tempChats = getCopyOfChats();
+			// makes more space if needed (2x)
+			if (numChats >= tempChats.length) {
+				Chat[] newChats = new Chat[tempChats.length * 2];
+				for (int i = 0; i < tempChats.length; i++) {
+					newChats[i] = tempChats[i];
+				}
+				tempChats = newChats;
 			}
-			chats = newChats;
+
+			tempChats[numChats] = chat;
+			reorderList(tempChats, numChats);
+			chats = tempChats;
+			numChats++;
 		}
-
-		chats[numChats] = chat;
-		reorderList(numChats++);
 	}
-	
 
-	//getters
+	// getters
 	public TextMessage getChatMessage(int chatIndex, int messageIndex) {
 		return chats[chatIndex].getMessage(messageIndex);
 	}
-	
+
 	public int getChatMemberId(int chatIndex, int memberIndex) {
 		return chats[chatIndex].getMemberId(memberIndex);
 	}
-	
+
 	public Instant getChatNewestUpdate(int chatIndex) {
 		return chats[chatIndex].getNewestUpdate();
 	}
-	
+
 	public Chat getChat(int chatIndex) {
 		return chats[chatIndex];
 	}
-	
 
-	//add message
-	public synchronized void addChatMessage(int chatIndex, TextMessage message) {
-		chats[chatIndex].addMessage(message);
-		reorderList(chatIndex);
+	// add message
+	public void addChatMessage(int chatIndex, TextMessage message) {
+		synchronized (writeMutex) {
+			Chat[] tempChats = getCopyOfChats();
+			tempChats[chatIndex].addMessage(message);
+			reorderList(tempChats, chatIndex);
+			chats = tempChats;
+		}
 	}
 
-	//attempt to add a member to a chat
+	// attempt to add a member to a chat
 	public void addChatMember(int chatIndex, int memberId, int fromId) {
-		//confirm that the user is the chat owner in a group chat
+		// confirm that the user is the chat owner in a group chat
 		Chat chat = chats[chatIndex];
-		if(chat.getChatType() == ChatType.PRIVATE)
+		if (chat.getChatType() == ChatType.PRIVATE)
 			return;
-		if(chat.getCreatorId() != fromId)
+		if (chat.getCreatorId() != fromId)
 			return;
-		chat.addMember(memberId); //add member
+		chat.addMember(memberId); // add member
 	}
-	
 
-	//attempt to remove a member from a chat
+	// attempt to remove a member from a chat
 	public void removeChatMember(int chatIndex, int memberId, int fromId) {
-		//confirm that the user is the chat owner in a group chat
+		// confirm that the user is the chat owner in a group chat
 		Chat chat = chats[chatIndex];
-		if(chat.getChatType() == ChatType.PRIVATE)
+		if (chat.getChatType() == ChatType.PRIVATE)
 			return;
-		if(chat.getCreatorId() != fromId)
+		if (chat.getCreatorId() != fromId)
 			return;
-		chat.removeMember(memberId); //remove member
+		chat.removeMember(memberId); // remove member
 	}
-	
 
-	//Attempts the delete the chat with id "chatId"
+	// Attempts the delete the chat with id "chatId"
 	public void deleteChat(int chatId, int fromId) {
-		int indexInArray = 0; // attempts to find the chat to delete
-		while(indexInArray < numChats) {
-			if(chats[indexInArray].getChatId() == chatId) {
-				break; // break if find
+		synchronized (writeMutex) {
+			Chat[] tempChats = getCopyOfChats();
+			int indexInArray = 0; // attempts to find the chat to delete
+			while (indexInArray < numChats) {
+				if (tempChats[indexInArray].getChatId() == chatId) {
+					break; // break if find
+				}
+				indexInArray++;
 			}
-			indexInArray++;
+			if (indexInArray >= numChats) {
+				return; // Do nothing if not in array
+			}
+			// confirm that the user is the chat owner in a group chat
+			Chat chat = tempChats[indexInArray];
+			if (chat.getChatType() == ChatType.PRIVATE)
+				return;
+			if (chat.getCreatorId() != fromId)
+				return;
+			// remove the chat
+			for (int i = indexInArray; i < (numChats - 1); i++) {
+				tempChats[i] = tempChats[i + 1]; // shift array down 1
+			}
+			chats = tempChats;
+			numChats--; // decrement numchats
 		}
-		if(indexInArray >= numChats) {
-			return; // Do nothing if not in array
-		}
-		//confirm that the user is the chat owner in a group chat
-		Chat chat = chats[indexInArray];
-		if(chat.getChatType() == ChatType.PRIVATE)
-			return;
-		if(chat.getCreatorId() != fromId)
-			return;
-		//remove the chat
-		for(int i = indexInArray; i < (numChats - 1); i++) {
-			chats[i] = chats[i+1]; // shift array down 1
-		}
-		numChats--; // decrement numchats
 	}
-	
 
-	//returns a string of all chat ids in the list separated by ','
+	// returns a string of all chat ids in the list separated by ','
 	public String toString() {
 		String retStr = "";
-		for(int i = 0; i < numChats; i++) {
-			if(i != 0)
+		for (int i = 0; i < numChats; i++) {
+			if (i != 0)
 				retStr += ',';
 			retStr += chats[i].getChatId();
 		}
 		return retStr;
 	}
 
-	private synchronized void reorderList(int updatedChatIndex) {
-		if(updatedChatIndex == 0)
+	private void reorderList(Chat[] tempChats, int updatedChatIndex) {
+		if (updatedChatIndex == 0)
 			return;
 		int i;
-		for(i = 0; i < updatedChatIndex; i++) {
-			if(chats[i].getNewestUpdate()
-					.compareTo(chats[updatedChatIndex].getNewestUpdate()) <= 0)
+		for (i = 0; i < updatedChatIndex; i++) {
+			if (tempChats[i].getNewestUpdate().compareTo(tempChats[updatedChatIndex].getNewestUpdate()) <= 0)
 				break;
 		}
-		if(i == updatedChatIndex)
+		if (i == updatedChatIndex)
 			return;
-		Chat tempChat = chats[updatedChatIndex];
-		for(int j = updatedChatIndex; j > i; j--) {
-			chats[j] = chats[j - 1];
+		Chat tempChat = tempChats[updatedChatIndex];
+		for (int j = updatedChatIndex; j > i; j--) {
+			tempChats[j] = tempChats[j - 1];
 		}
-		chats[i] = tempChat;
+		tempChats[i] = tempChat;
+	}
+
+	private Chat[] getCopyOfChats() {
+		Chat[] newChats = new Chat[chats.length];
+		for (int i = 0; i < numChats; i++) {
+			newChats[i] = chats[i];
+		}
+		return newChats;
 	}
 }
